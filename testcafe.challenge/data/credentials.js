6dd519d6beab12f2a934a723ac281fb2b3609981
pandreaflo <pamela_flores@intuit.com>
2021-04-21 16:05:53 -0500

    adding credentials

--- testcafe.challenge/data/credentials.js
+++ testcafe.challenge/data/credentials.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
+export const CREDENTIALS = {
+  STANDARD_USER: {
+    USERNAME: process.env.VALID_USER,
+    PASSWORD: process.env.PWD
+  }, INVALID_USER: {
+    USERNAME: process.env.INVALID_USER,
+    PASSWORD: process.env.PWD
+  }
 
-
-
+}
