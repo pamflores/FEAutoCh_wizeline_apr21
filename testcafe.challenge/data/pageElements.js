f16e0ad731af4eefec48bdebe046606cab9b4356
pandreaflo <pamela_flores@intuit.com>
2021-04-22 16:56:33 -0500

    adding login with invalid creds

--- testcafe.challenge/data/pageElements.js
+++ testcafe.challenge/data/pageElements.js
@@ -7,6 +7,9 @@ export const PAGE = {
         PRODUCT: "PRODUCTS",
         YOUR_CART: "YOUR CART",
         OVERVIEW: "CHECKOUT: OVERVIEW"
-  }
+  },
+    PAGEMSGS: {
+        LOGINERROR: "Epic sadface: Username and password do not match any user in this service"
+    }
     
 }
--- testcafe.challenge/tests/Login.js
+++ testcafe.challenge/tests/Login.js
@@ -12,3 +12,14 @@ test(`Login with valid credentials`, async t => {
     await loginpage.loginMethod(CREDENTIALS.STANDARD_USER.USERNAME, CREDENTIALS.STANDARD_USER.PASSWORD)
     await t.expect(await productListing.returnPLPtitle()).eql(PAGE.PAGETITLES.PRODUCT) //Verify user is logged in
 });
+test(`Login with invalid credentials`, async t => {
+    //await t.useRole(roles.invalidUser) //Login with invalid user with roles
+    await loginpage.loginMethod(CREDENTIALS.INVALID_USER.USERNAME, CREDENTIALS.INVALID_USER.PASSWORD)
+    await t.expect(await loginpage.getErrorMsg()).eql(PAGE.PAGEMSGS.LOGINERROR) //Verify error is displayed
+});
+test(`Logout`, async t => {
+    //await t.useRole(roles.validUser) //Login with valid user with roles
+    await loginpage.loginMethod(CREDENTIALS.STANDARD_USER.USERNAME, CREDENTIALS.STANDARD_USER.PASSWORD)
+    await t.expect(await productListing.returnPLPtitle()).eql(PAGE.PAGETITLES.PRODUCT) //Verify user is logged in
+    // TO DO: Insert here the logout code
+});
